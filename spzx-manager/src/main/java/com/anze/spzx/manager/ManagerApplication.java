d7eb83dcfa8ac6233604fbb9a9fcaa15e60e1a79
Anze <2290434225@qq.com>
2023-12-04 20:24:16 +0800

    d1

--- spzx-manager/src/main/java/com/anze/spzx/manager/ManagerApplication.java
+++ spzx-manager/src/main/java/com/anze/spzx/manager/ManagerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan()
 public class ManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManagerApplication.class,args);
