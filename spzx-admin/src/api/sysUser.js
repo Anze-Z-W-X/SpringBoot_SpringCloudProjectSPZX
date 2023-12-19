ffc0282a988931b64694254729119e07d37c76c6
Anze <2290434225@qq.com>
2023-12-19 10:54:59 +0800

    d8

---
+++ spzx-admin/src/api/sysUser.js
@@ -0,0 +1,38 @@
+import request from '@/utils/request'
+
+const base_api = '/admin/system/sysUser'
+
+// 用户列表
+export const GetSysUserListByPage = (current , limit , queryDto) => {
+    return request({
+        url: `${base_api}/findByPage/${current}/${limit}` ,
+        method: 'get',
+        params: queryDto
+    })
+}
+
+//用户添加
+export const SaveSysUser = (sysUser)=>{
+    return request({
+        url: `${base_api}/saveSysUser`,
+        method: 'post',
+        data: sysUser
+    })
+}
+
+//用户修改
+export const UpdateSysUser = (sysUser)=>{
+    return request({
+        url: `${base_api}/updateSysUser`,
+        method: 'put',
+        data: sysUser
+    })
+}
+
+//用户删除
+export const DeleteById = (userId)=>{
+    return request({
+        url: `${base_api}/deleteById/${userId}`,
+        method: 'delete'
+    })
+}
